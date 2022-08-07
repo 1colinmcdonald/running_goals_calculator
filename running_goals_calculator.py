f85e22fc6ad28fc0fc6b857ec78cf703a47eddef
Colin McDonald <89328592+geosyog3@users.noreply.github.com>
2022-08-06 17:31:32 -0700

    Add files via upload

---
+++ running_goals_calculator.py
@@ -0,0 +1,31 @@
+def calculator():
+    mileage_goal = int(input("How many miles do you want to run this week?\n"))
+    mileage_so_far = float(input("How many miles have you run so far?\n"))
+    days_left = int(input("How many days of the week are left?\n"))
+    long_run = mileage_goal / 4.0
+    if long_run - int(long_run) >= .5:
+        long_run = int(long_run) + 1
+    else:
+        long_run = int(long_run)
+        
+    regular_run = (mileage_goal - long_run - mileage_so_far) / (days_left - 1)
+    shorter_regular_run = int(regular_run)
+    longer_regular_run = shorter_regular_run
+    if regular_run % int(regular_run) != 0:
+        longer_regular_run = int(regular_run) + 1
+    print(round((regular_run % int(regular_run)) * (days_left - 1)))
+    days_of_longer_regular_run = round((regular_run % int(regular_run)) * (days_left - 1))    
+    
+    
+    days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    for i in range(days_left - days_of_longer_regular_run - 1):
+        print("run", shorter_regular_run, "miles on", days_of_the_week[i + 7 - days_left])
+    for j in range(days_of_longer_regular_run):
+        print("run", longer_regular_run, "miles on", days_of_the_week[j + 7 - days_left - days_of_longer_regular_run])
+    print("run", long_run, "miles on", "Sunday")
+    
+    print(days_of_longer_regular_run)
+
+
+if __name__ == '__main__':
+    calculator()
