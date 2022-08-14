af89b1ea7195797cbe22fc419122bb9c9dbd64f3
geosyog3 <1colinmcdonald@gmail.com>
2022-08-14 01:51:11 -0700

    8/14 1:51

--- running_goals_calculator.py
+++ running_goals_calculator.py
@@ -13,19 +13,18 @@ def calculator():
     longer_regular_run = shorter_regular_run
     if regular_run % int(regular_run) != 0:
         longer_regular_run = int(regular_run) + 1
-    print(round((regular_run % int(regular_run)) * (days_left - 1)))
+    
     days_of_longer_regular_run = round((regular_run % int(regular_run)) * (days_left - 1))    
-    
-    
+    days_of_shorter_regular_run = days_left - days_of_longer_regular_run - 1
+    print(days_of_shorter_regular_run)
     days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     for i in range(days_left - days_of_longer_regular_run - 1):
         print("run", shorter_regular_run, "miles on", days_of_the_week[i + 7 - days_left])
     for j in range(days_of_longer_regular_run):
-        print("run", longer_regular_run, "miles on", days_of_the_week[j + 7 - days_left - days_of_longer_regular_run])
+        print("run", longer_regular_run, "miles on", days_of_the_week[j + 7 - days_left + days_of_shorter_regular_run])
     print("run", long_run, "miles on", "Sunday")
     
-    print(days_of_longer_regular_run)
-    print("hi")
+    
 
 
 if __name__ == '__main__':
